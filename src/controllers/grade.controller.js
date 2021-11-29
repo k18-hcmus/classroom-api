54b0ffabea943c876d558449cfb90f0020dee8ce
nguyen315 <hominhnguyen315@gmail.com>
2021-11-29 13:23:51 +0700

    [CLR-017] Delete Assignment API

--- src/controllers/grade.controller.js
+++ src/controllers/grade.controller.js
@@ -1,8 +1,9 @@
 import BaseCtrl from './base'
 import db from '../models/index'
-import { controller, get, post, put } from 'route-decorators'
+import { controller, get, post, del } from 'route-decorators'
 import httpStatusCodes from 'http-status-codes'
 import { auth } from '../middleware'
+import debug from 'src/utils/debug'
 
 @controller('/api/classrooms/:id/grades')
 class GradesCtrl extends BaseCtrl {
@@ -28,6 +29,36 @@ class GradesCtrl extends BaseCtrl {
       console.log(error)
     }
     res.status(httpStatusCodes.OK).send(grade)
+  }
+
+  @get('/', auth())
+  async getGrades(req, res) {
+    const { id: classroomId } = req.params
+
+    let grades
+
+    try {
+      grades = await db.Grade.findAll({
+        where: {
+          classroomId,
+        },
+      })
+    } catch (error) {
+      debug.log('grade-ctrl', error)
+    }
+
+    res.status(httpStatusCodes.OK).send(grades)
+  }
+
+  @del('/:gradeId', auth())
+  async deleteGrade(req, res) {
+    const { gradeId } = req.params
+    await db.Grade.destroy({
+      where: {
+        id: gradeId,
+      },
+    })
+    res.status(httpStatusCodes.OK).send({ message: 'Delete assignment successful' })
   }
 }
 export default GradesCtrl
